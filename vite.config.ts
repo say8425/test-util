2a74506bdaa7668d468a6df50c9df304cfd3feba
Penguin <say8425@gmail.com>
2024-04-03 15:39:14 +0900

    initialize commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "TestUtil",
+      fileName: "main",
+    },
+    rollupOptions: {
+      output: {
+        globals: {
+          testUtil: "TestUtil",
+        },
+      },
+    },
+  },
+});
